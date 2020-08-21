c28903857ff5328f5577a316800ccb1a07d382dc
MVS Sriharsha <sriharshamvs@gmail.com>
2020-08-21 13:13:31 +0530

    Assest & CSS

--- src/pages/posts/first-post.js
+++ src/pages/posts/first-post.js
@@ -1,8 +1,15 @@
 import Link from "next/link"
+import Head from "next/head"
+import Layout from "../components/layout"
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>
+          First Post
+        </title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
@@ -11,6 +18,6 @@ export default function FirstPost() {
           </a>
         </Link>
       </h2>
-    </>
+    </Layout>
   )
 }
