583f64abf4d9c8306cdc2ed63a49f0ef358c2571
MVS Sriharsha <sriharshamvs@gmail.com>
2020-08-22 14:41:51 +0530

    updated index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,10 +20,6 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hello, I'm Sriharsha. I'm a software engineer and I tinker with things. Contact me on <a href="https://twitter.com/sriharshamvs">Twitter</a></p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
       <section className={utilStyles.headingMd}>…</section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
