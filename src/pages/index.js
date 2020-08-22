6d59bc84252212963724787c8382dd223f23fc1a
MVS Sriharsha <sriharshamvs@gmail.com>
2020-08-22 14:39:58 +0530

    update index.js file

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Sriharsha. I'm a software engineer and a I like to tinker with things. Contact me on <a href="https://twitter.com/sriharshamvs">Twitter</a></p>
+        <p>Hello, I'm Sriharsha. I'm a software engineer and I tinker with things. Contact me on <a href="https://twitter.com/sriharshamvs">Twitter</a></p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
