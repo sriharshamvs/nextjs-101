a64989db093778952dd11d88fe0951d987ca65ae
MVS Sriharsha <sriharshamvs@gmail.com>
2020-08-23 08:47:28 +0530

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   target: 'serverless',
   webpack: (config) => {
-    config.resolve.alias['~'] = path.resolve(__dirname, 'src')
+    config.resolve.alias['~'] = path.resolve(__dirname, 'src/pages')
     return config
   },
   env: {},
